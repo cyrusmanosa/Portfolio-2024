ff6fa89ba25aec0d5fb4a0bcf1a000a121eabfdd
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2024-07-27 15:34:49 +0900

    update Care Connect github link

--- src/WebVersion/IT/GitBtn.jsx
+++ src/WebVersion/IT/GitBtn.jsx
@@ -9,11 +9,11 @@ import styled from '@emotion/styled';
 function MyDialog() {
     const [open, setOpen] = useState(false);
     const [gitData, setGitData] = useState([]);
-    const apiurl = "https://api.github.com/users/cyrusmanosa/repos";
+    const apiMyselfUrl = "https://api.github.com/users/cyrusmanosa/repos";
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await Axios.get(apiurl);
+                const response = await Axios.get(apiMyselfUrl);
                 const filteredData = response.data
                     .filter((repo) => repo.private === false)
                     .filter((repo) => repo.owner.login === "cyrusmanosa")
@@ -37,20 +37,26 @@ function MyDialog() {
     };
 
     const GitBtn = styled(Button)`
-    display: flex;
-    text-align: center;
-    width: 100%;
-    background-color: #fff;
-    padding: 1%;
-    font-size: 130%;
-    border-radius: 0;
-    text-transform: capitalize;
-    border-top: 2.5px solid #000;
-    border-bottom: 2.5px solid #000;
-    color:#000;
-    font-family: "Paytone One", sans-serif;
-    font-style: normal;
+        display: flex;
+        text-align: center;
+        width: 100%;
+        background-color: #fff;
+        padding: 1%;
+        font-size: 130%;
+        border-radius: 0;
+        text-transform: capitalize;
+        border-top: 2.5px solid #000;
+        border-bottom: 2.5px solid #000;
+        color:#000;
+        font-family: "Paytone One", sans-serif;
+        font-style: normal;
     `;
+    const DialogOutline = styled.div`
+        width: 400px;
+        display:flex;
+        flex-direction: column;
+        justify-content: center;
+    `
 
     return (
         <>
@@ -59,16 +65,23 @@ function MyDialog() {
             </GitBtn>
 
             <Dialog open={open} onClose={() => setOpen(false)} className="DialogStyle">
-                <DialogTitle>Cyrus Man</DialogTitle>
-                <DialogContent className='DialogContentStyle'>
-                    {gitData.map((repo) => (
-                        <div key={repo.name}>
-                            <Button onClick={() => HandleOpenUrl(repo.html_url)} variant="contained">
-                                {repo.name}
-                            </Button>
-                        </div>
-                    ))}
-                </DialogContent>
+                <DialogOutline>
+                    <DialogTitle style={{ fontWeight: 900, fontSize: 20, margin: 0 }}>Cyrus Man</DialogTitle>
+                    <DialogContent className='DialogContentStyle'>
+                        {gitData.map((repo) => (
+                            <div key={repo.name}>
+                                <Button onClick={() => HandleOpenUrl(repo.html_url)} variant="contained">
+                                    {repo.name}
+                                </Button>
+                            </div>
+                        ))}
+
+                        <DialogTitle style={{ fontWeight: 900, fontSize: 20, margin: 0 }}>Organizations</DialogTitle>
+                        <Button onClick={() => HandleOpenUrl("https://github.com/ECC200")} variant="contained">
+                            Care Connect
+                        </Button>
+                    </DialogContent>
+                </DialogOutline>
             </Dialog>
         </>
     );
