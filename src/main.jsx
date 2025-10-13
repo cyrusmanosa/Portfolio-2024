9d41c292e7b864cb85cf84610be25cd5668195a0
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2025-10-14 02:42:55 +0800

    Delete Demo folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Login from './WebVersion/Demo/Login.jsx'
 import './index.css'
 import './i18.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    {/* <Login /> */}
   </React.StrictMode>
 )
